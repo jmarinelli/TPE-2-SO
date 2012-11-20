2f5f3ce942ef7b50e68a59fef26884d6758733f0
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-11-21 00:47:19 +0100

    Agregado path del client

--- src/server/server.c
+++ src/server/server.c
@@ -20,7 +20,7 @@ void server_close(void);
 int main() {
 	
 	instruction_header_t client_header = calloc(1, sizeof(instruction_header));
-	string instruction_string;
+	string instruction_string, client_working_dir, parameter;
 	
 	signal(SIGINT, (__sighandler_t)server_close);
 	
@@ -42,18 +42,28 @@ int main() {
 	while(1) {
 		if(read(comChannel, client_header, sizeof(struct instruction_header)) > 0) {
 			instruction_string = calloc(1, client_header->instruction_size);
+			client_working_dir = calloc(1, client_header->current_path_size + 1);
+			parameter = calloc(1, client_header->parameter_size);
 
 			while(read(comChannel, instruction_string, client_header->instruction_size) == 0)
 				sleep(1);
+			while(read(comChannel, client_working_dir, client_header->current_path_size) == 0)
+				sleep(1);
+			client_working_dir[client_header->current_path_size] = 0;
+			if (client_header->parameter_size > 0) {
+				while(read(comChannel, parameter, client_header->parameter_size) == 0)
+					sleep(1);
+				strcat(instruction_string, " ");
+				strcat(instruction_string, parameter);
+			}
+			
+			printf("Received instruction from client: %d, instruction: %s, working dir %s\n",
+				client_header->client_id, instruction_string, client_working_dir);
 				
-			printf("Received instruction from client: %d, instruction: %s\n",
-				client_header->client_id, instruction_string);
-				
-			if (parse_string(instruction_string) == -1) {
+			if (parse_string(instruction_string, client_working_dir) == -1) {
 				printf("%s not a valid instruction\n", instruction_string);
 			}
-				
-			free(instruction_string);
+			
 		}
 		sleep(1);
 	}
