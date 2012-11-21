fc5957ff700ee6ab666d96536884294939a5f527
juanjo <juanjo@ubuntu.(none)>
2012-11-21 00:33:33 -0300

    branch tree hecho

--- include/filesystem/fstree.h
+++ include/filesystem/fstree.h
@@ -2,3 +2,4 @@ fstree_t new_fstree ( void );
 error add_child ( fstree_node_t , fstree_node_t );
 error add_directory ( fstree_node_t , fstree_node_t );
 fstree_node_t new_fstree_node ( bool , string , unsigned );
+fstree_t branch_tree ( fstree_t );
